ed3f03f1d9dab5d57677274eb76cedff4087a41b
Ciervos <55422681+Ciervos@users.noreply.github.com>
2020-09-02 20:25:57 -0300

    parte funcional v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Todolistsincall from './components/todolistsincall/index';
+import React, { useState } from "react";
+import AddTask from "./components/todolistcall/AddTask/index";
+import Tasks from "./components/todolistcall/Tasks/index";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  function handleInputValue(value) {
+    setTasks([
+      ...tasks,
+      {
+        value,
+        isCompleted: false
+      }
+    ]);
+  }
+
   return (
-<div>
-  <Todolistsincall/>
-  </div>
+    <>
+      <AddTask handleCallback={handleInputValue} />
+      <Tasks tasks={tasks} />
+      Tareas resultas:
+      <ul>
+        <li>ACA VAN</li>
+      </ul>
+    </>
   );
 }
 
